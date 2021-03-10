052a5d53a843e60fdf1a481a35fad776924ccd35
Paresh Mahyavanshi <pmahyavanshi13@gmail.com>
2021-03-10 10:01:23 -0800

    Task_5 Completed

--- Task_5/Que_5.py
+++ Task_5/Que_5.py
@@ -1,3 +1,18 @@
 #
 # 5. Go through the link provided below to understand finally and raise concept: https://www.programiz.com/python-programming/exception-handling
+#
 # 
+
+try:
+    usr_ip = int(input("Enter four digits number :"))
+    ip = str(usr_ip)
+    if len(ip)!=4:
+        raise ValueError ("Worng Number of Digit")
+    else: 
+        print("Number of digit are good")
+except ValueError:
+     print("The length is too short/long !!!")
+finally:
+    print("Now, you are at the end program!")
+
+
